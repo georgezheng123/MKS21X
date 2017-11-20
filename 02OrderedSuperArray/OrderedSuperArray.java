343c5e4c21972b40c27880a6f7caab58ee1e9225
George <gzheng3@stuy.edu>
2017-11-20 14:31:03 -0500

    added all

---
+++ 02OrderedSuperArray/OrderedSuperArray.java
@@ -0,0 +1,105 @@
+public class OrderedSuperArray extends SuperArray{
+
+  /*constructor initializes an empty List*/
+  public OrderedSuperArray(){
+    super();
+  }
+
+  public OrderedSuperArray(int len){
+    super(len);
+  }
+
+  public OrderedSuperArray(String [] ary){
+    super();
+    for (int i=0; i<ary.length; i++){
+      add(ary[i]);
+    }
+  }
+
+public String set(int index, String element){
+      throw new UnsupportedOperationException();
+  }
+
+  /*call the proper add.*/
+  public boolean add(int index, String value){
+    return add(value);
+  }
+
+  /*Write this method and any
+    appropriate helper methods.*/
+  public boolean add(String value){
+    for (int i=0; i<size(); i++){
+      if (value.compareTo(get(i)) < 0) {
+        super.add(i, value);
+        return true;
+      }
+    }
+    super.add(value);
+  
+    return true;
+  }
+
+  public int indexOf(String element){
+    int first = 0, last = size() - 1;
+    while (first <= last){
+      int middle = first + (last-first)/2;
+      if (get(middle).compareTo(element) == 0){
+        while (middle > 0 && get(middle-1)==element){
+          middle -= 1;
+        }
+        return middle;
+      }
+      if (get(middle).compareTo(element) < 0) first = middle + 1;
+      else last = middle - 1;
+    } 
+    return -1;
+  }
+
+    public int indexOfBinary(String element){
+      return indexOf( element);
+}
+
+  public int lastIndexOf(String element){
+    int first = 0, last = size() - 1;
+    while (first <= last){
+      int middle = first + (last-first)/2;
+      if (get(middle).compareTo(element) == 0){
+        while (middle < size()-1 && get(middle+1)==element){
+          middle += 1;
+        }
+        return middle;
+      }
+      if (get(middle).compareTo(element) < 0) first = middle + 1;
+      else last = middle - 1;
+    }
+    return -1;
+  }
+
+  public int lastIndexOfBinary(String element){
+    return lastIndexOf(element);
+  }
+
+  public int findIndex(String value){
+    for (int i=0; i<size(); i++){
+      if (value.compareTo(get(i)) < 0) {
+        return i;
+      }
+    }  
+    return size();
+  }
+
+    public int findIndexBinary(String element){
+      if (!super.contains(element)) return -1;
+    int first = 0, last = size() - 1;
+    while (first <= last){
+      int middle = first + (last-first)/2;
+      if (get(middle).compareTo(element) == 0){
+        return middle;
+      }
+      if (get(middle).compareTo(element) < 0) first = middle + 1;
+      else last = middle - 1;
+    } 
+    return first;
+  }
+
+}
---
+++ 02OrderedSuperArray/SuperArray.java
@@ -0,0 +1,164 @@
+public class SuperArray{ 
+
+    private String[] data;
+    private int size;
+    
+    public SuperArray(){
+	data = new String[10]; 
+	size = 0;
+    }
+
+    public void clear(){
+	size = 0;
+    }
+    
+    public int size(){
+	return this.size;
+    }
+
+    public boolean isEmpty(){
+	for (int i=0; i<size; i++){
+	    if (data[i] != null) return false;
+	}
+	return true;
+    }
+
+    public boolean add(String element){
+	String[] updated = new String[size + 1];
+	for (int i=0; i<size; i++){
+	    updated[i] = data[i];
+	}
+	updated[size] = element;
+	this.data = updated;
+	size++;
+	return true;
+    }
+
+    public String get(int index){
+	if (index < 0 || index >= size()){
+	    System.out.println("out of range error");
+	    return null;
+	}
+	return data[index];
+    }
+
+    public String set(int index, String element){
+	if (index < 0 || index >= size()){
+	    System.out.println("out of range error");
+	    return null;
+	}
+	String[] updated = new String[size];
+	String toReturn = get(index);
+	for (int i=0; i<size; i++){
+	    if (i != index) {
+		updated[i] = data[i];
+	    }
+	    else {
+		updated[i] = element;
+	    }
+	}
+	data = updated;
+	return toReturn;
+    }
+
+    public String toString(){
+	if (size == 0){
+	    return "[]";
+	}
+	String ans = "[";
+	for (int i=0; i<size-1; i++){
+	    ans += data[i] + ",";
+	}
+	return ans + data[size-1] + "]";
+    }
+
+    public boolean contains(String element){
+	for (int i=0; i<size; i++){
+	    if (data[i] != null && data[i].equals(element)){
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    public void resize(){
+	int bigly = size*2+2;
+	String[] updated = new String[bigly];
+	for (int i=0; i<size; i++){
+	    updated[i] = data[i];
+	}
+	size = bigly;
+	data = updated;
+    }
+    
+    public void add(int index, String element){
+	if (index < 0 || index >= size || index >= data.length){
+	    System.out.println("out of range error");
+	}
+	else{
+	String[] updated = new String[size+1];
+	for (int i=0; i<index; i++){
+	    updated[i] = data[i];
+	}
+	for (int i=index; i<size; i++){
+	    updated[i+1] = data[i];
+	}
+	updated[index] = element;
+	data = updated;
+	size += 1;}
+    }
+
+    public int indexOf(String element){
+	if (!contains(element)) {
+	    return -1;
+	}
+	for (int i=0; i<size; i++){
+	    if (data[i] != null && data[i].equals(element)){
+		return i;
+	    }
+	}
+	return size-1;
+    }
+
+     public int lastIndexOf(String element){
+	if (!contains(element)) {
+	    return -1;
+	}
+	for (int i=size-1; i>=0; i--){
+	    if (data[i] != null && data[i].equals(element)){
+		return i;
+	    }
+	}
+	return 0;
+    }
+
+    public String remove(int index){
+	if (index < 0 || index >= size()){
+	    System.out.println("out of range error");
+	    return null;
+	}
+	String toReturn = data[index];
+	for (int i = index; i< size-1; i++){
+	    data[i] = data[i+1];
+	}
+	String[] updated = new String[size-1];
+	for (int i=0; i<size-1; i++){
+	    updated[i] = data[i];
+	}
+	data = updated;
+	size -= 1;
+	return toReturn;
+	
+	
+    }
+
+    public Boolean remove(String target){
+	int index = indexOf(target);
+	if (index == -1) {
+	    return false;
+	}
+	this.remove(index);
+	return true;
+    }
+}	
+
