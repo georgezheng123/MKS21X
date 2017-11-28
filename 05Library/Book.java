a8f1df17f4da2cd0d6b820627aa8a03e617abffd
George <gzheng3@stuy.edu>
2017-11-28 15:21:42 -0500

    toString

--- 05Library/Book.java
+++ 05Library/Book.java
@@ -1,7 +1,21 @@
-public Class Book{
+public class Book{
 
     private String author;
     private String ISBN;
     private String title;
 
+    public Book(){
+    }
+
+    public Book(String author, String ISBN, String title){
+	this.author = author;
+	this.ISBN = ISBN;
+	this.title = title;
+    }
+
+    public String toString(){
+	return title + "," +  author + "," +  ISBN;
+    }
+
+    
 }
