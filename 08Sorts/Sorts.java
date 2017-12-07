db7213b770bcbcf75a7e38222fb1d432b88b196b
George Zheng <georgezheng999@gmail.com>
2017-12-07 09:35:07 -0500

    added sorts

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,34 +1,19 @@
+import java.util.Arrays;
+
 public class Sorts{
 
   public static void main(String[]args){
-    int[] a = {64, 25, 12, 22, 11};
+    //int[] a = {64, 25, 12, 22, 11};
     //int[] a = {34, 25, 12, 22, 12};
     //int[] a = {6, 25, -12, 22, 1, 32, 22};
     //int[] a = {-5, -25, -12, -15, 1};
     //int[] a = {1, 1, 0, 1, 0, 0};
-    //int[] a = {1932, -25, 12, 12, 3};
+    int[] a = {1932, -25, 12, 12, 3};
     //int[] a = {4, 23, -12, 0, 11, 1, 4, 99, 32, 14, 19, 23};
 
-    String input = "[";
-    for (int i=0;i<a.length;i++){
-      input += a[i];
-      if (i!=a.length-1){
-        input += ",";
-      }
-    }
-    input += "]";
-    System.out.println(input);
-    selectionSort(a);
-    System.out.println("-----Sorted Below----");
-    String output = "[";
-    for (int i=0;i<a.length;i++){
-      output += a[i];
-      if (i!=a.length-1){
-        output += ",";
-      }
-    }
-    output += "]";
-    System.out.println(output);
+    System.out.println(Arrays.toString(a));
+    insertionSort(a);
+    System.out.println(Arrays.toString(a));
   }
 
 
@@ -48,5 +33,40 @@ public class Sorts{
         }
     }
   }
+
+  public static void insertionSort(int[] data){ 
+    int j = 1; 
+    while (j < data.length) {  
+        int current = data[j];  
+        int i = j-1;  
+        while (i > -1 && data[i] > current) {  
+            data[i+1] = data[i];  
+            i--;  
+        }  
+        data[i+1] = current;  
+        j++;
+    }  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
