288d9e34ba793caa14ed244213908c34ccdc9abe
George Zheng <georgezheng999@gmail.com>
2017-12-11 15:15:05 -0500

    bug fix on bubble sort

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -48,9 +48,9 @@ public class Sorts{
     }  
 }
     public static void bubbleSort(int arr[]){
-        for (int i = 0; i <arr.length-1; i++)
-            for (int j = 0; j<arr.length-i-1; j++)
-                if (arr[j+1] < arr[j]){
+        for(int i = 0; i <arr.length-1; i++)
+            for(int j = 0; j<arr.length-i-1; j++)
+                if (arr[j+1] <= arr[j]){
                     int holder = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = holder;
