ad20316cc152f450f35e12c02c434a17ca59ada0
George Zheng <georgezheng999@gmail.com>
2017-12-14 14:35:33 -0500

    forgot to push

--- 09Gui/Window.java
+++ 09Gui/Window.java
@@ -1,43 +1,55 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 public class Window extends JFrame implements ActionListener{
- private Container pane;
 
- private JButton b;
- private JLabel l;
+ private JButton FtoC;
+ private JButton CtoF;
+ private Container pane;
  private JTextField t;
- private JCheckBox c;
- 
+
+  //MAIN JUST INSTANTIATES + MAKE VISIBLE
+ public static void main(String[] args) {
+     Window g = new Window();
+     g.setVisible(true);
+ }
+
   //CONSTRUCTOR SETS EVERYTHING UP
-  public Window() {
-     this.setTitle("My first GUI");
+ public Window() {
+     this.setTitle("Convert FtoC and CtoF");
      this.setSize(600,400);
      this.setLocation(100,100);
      this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    
+
      pane = this.getContentPane();
      pane.setLayout(new FlowLayout());  
-    
-     b = new JButton("toF");
-     l = new JLabel("Temperature Converter",null,JLabel.CENTER);
+
      t = new JTextField(12);
-     pane.add(l);
-     pane.add(b);
+     FtoC = new JButton("FtoC");
+     CtoF = new JButton("CtoF");
+
+     t.addActionListener(this);
      pane.add(t);
+
+     FtoC.addActionListener(this);
+     pane.add(FtoC);
+
+     CtoF.addActionListener(this);
+     pane.add(CtoF);
  }
 
- public double CtoF(double t){
-    return (9.0/5.0)*t + 32;
+ public static double CtoF(double t){
+    return 1.8*t + 32;
 }
 
-public double FtoC(double t){
-    return (5.0/9.0)*t - 32;
+public static double FtoC(double t){
+    return (5.0/9.0)*(t - 32);
 }
 
+public void actionPerformed(ActionEvent e){
+    String s = e.getActionCommand();
+    if (s.equals("CtoF")) t.setText(CtoF(Double.parseDouble(t.getText()))+"");
+    if (s.equals("FtoC")) t.setText(FtoC(Double.parseDouble(t.getText()))+"");
+}
 
-  //MAIN JUST INSTANTIATES + MAKE VISIBLE
-  public static void main(String[] args) {
-     Window g = new Window();
-     g.setVisible(true);
-  }
 }
