241cd1227832b86b7a78df8bb2881442322dcf8d
George Zheng <georgezheng999@gmail.com>
2018-01-03 16:36:34 -0500

    rolled some aspects over to string

--- 04ReversedCharSeq/ReversedCharSequence.java
+++ 04ReversedCharSeq/ReversedCharSequence.java
@@ -2,14 +2,20 @@ import java.lang.CharSequence;
 
 public class ReversedCharSequence implements CharSequence{
 
+	String s1 = "";
 	char[] arr;
 
-	public ReversedCharSequence(char[] grr){
-		arr = grr;
+	public ReversedCharSequence(String grr){
+		s1 = "";
+		int end = grr.length()-1;
+		for (int i=end; i > -1; i--){
+	    	s1 += grr.charAt(i);
+		}
+		arr = s1.toCharArray();
 	}
 
 	public int length(){
-		return arr.length;
+		return (arr).length;
 	} 
 	
 	public char charAt(int index){
@@ -18,21 +24,26 @@ public class ReversedCharSequence implements CharSequence{
 	}
 
 	public ReversedCharSequence subSequence(int start, int end){
-		char[] blah = new char[end - start];
-		for (int i=0; i<end-start; i++){
-			blah[i] = arr[i+start];
+		String holder = "";
+		for (int i=end-1; i >= start; i--){
+	    	holder += s1.charAt(i);
 		}
-		ReversedCharSequence ehh = new ReversedCharSequence(blah);
+		ReversedCharSequence ehh = new ReversedCharSequence(holder);
 		return ehh;
 
 	}
 
 	public String toString(){
-		String ans = "";
-		for (int i=arr.length-1; i>=0; i--){
-			ans += arr[i] + " ";
-		}
-		return ans;
+		return s1;
 	}
+
+	    public static void main(String[]args){
+	
+	ReversedCharSequence s = new ReversedCharSequence("hello world");
+	System.out.println(s.length() + ".... expecting 11");
+	System.out.println(s + ".... expecting dlrow olleh");
+	System.out.println(s.charAt(1) + ".... expecting l");
+	System.out.println(s.subSequence(2, 7) + ".... expecting row o");
+    }
 
 }
