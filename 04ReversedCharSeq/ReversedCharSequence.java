2854ec1140f219a5ebe547bba70ced734b535095
George Zheng <georgezheng999@gmail.com>
2018-01-04 17:37:25 -0500

    push of the day

--- 04ReversedCharSeq/ReversedCharSequence.java
+++ 04ReversedCharSeq/ReversedCharSequence.java
@@ -37,13 +37,5 @@ public class ReversedCharSequence implements CharSequence{
 		return s1;
 	}
 
-	    public static void main(String[]args){
-	
-	ReversedCharSequence s = new ReversedCharSequence("hello world");
-	System.out.println(s.length() + ".... expecting 11");
-	System.out.println(s + ".... expecting dlrow olleh");
-	System.out.println(s.charAt(1) + ".... expecting l");
-	System.out.println(s.subSequence(2, 7) + ".... expecting row o");
-    }
 
 }
